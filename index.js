9fd9e7a16649faa13cacb1345cf8afeb1bcfb4cd
Brian O'Carroll <bocarroll36@gmail.com>
2018-08-10 16:57:59 -0700

    added html form and basic query

---
+++ index.js
@@ -0,0 +1,15 @@
+const endpoint = "https://api.fda.gov/food/enforcement.json"
+//change product description to user input
+let JSONRequest = "https://api.fda.gov/food/enforcement.json?search=product_description:\"ice+cream\"&limit=25"
+function constructQuery(searchTerm, callback) {
+    const query = {
+        search: ,//limit search,
+        count: ,// hello
+        limit:
+    }
+}
+
+//https://api.fda.gov/food/event.json?search=outcomes:"serious+injuries"
+//results.reason_for_recall
+//results.product_quantity
+//results.product_description 
