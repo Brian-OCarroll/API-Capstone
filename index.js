186d7b4bfc7ad0cbd6d13a9e100b6ff25ba988e2
Brian O'Carroll <bocarroll36@gmail.com>
2018-08-14 10:34:39 -0700

    finished js for recall info

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const endpoint = "https://api.fda.gov/food/enforcement.json";
 //change product description to user input
 let JSONRequest = "https://api.fda.gov/food/enforcement.json?api_key:&search=product_description:\"ice+cream\"&limit=25"
-function constructQuery(searchTerm, callback) {
+function getDataFromAPI(searchTerm, callback) {
     const query = {
         api_key: 'F1QzPnq38QWsSLQzgprkG2UGycgQsqF7QMw4UWx6',
         search: `product_description:${searchTerm}`,
@@ -10,18 +10,36 @@ function constructQuery(searchTerm, callback) {
     $.getJSON(endpoint, query, callback);
 }
 
-function renderResult(search) {
+function renderResult(result) {
     return `
     <h2>Results</h2>
-    <h3>${results.product_description}</h3>
-    <p>${results.product_quantity}</p>
-    <p>${search.results.reason_for_recall}</p>
+    <h3>${result.product_description}</h3>
+    <p>${result.product_quantity}</p>
+    <p>${result.reason_for_recall}</p>
     `
 }
-function onSubmit(event) {
-    event.preventDefault();
+
+function displayRecallData(data) {
+    console.log(data)
+    /*for (let i =0; i<data.results[i];i++) {
+        renderResults(data.results[i])
+    }*/
+    //check to see if results is the right reference
+    //trying to use map on the returned json
+    const result = data.results.map((item, index) => renderResult(item));
+    $('.js-results').html(result)
 }
 
+function onSubmit() {
+   $('.js-search-form').on('submit', function(event){
+    event.preventDefault(event);
+    const queryTarget = $(this).find('.js-query');
+    const query = queryTarget.val();
+    queryTarget.val("");
+    getDataFromAPI(query, displayRecallData);
+   });
+}
+$(onSubmit);
 //https://api.fda.gov/food/event.json?search=outcomes:"serious+injuries"
 //https://api.fda.gov/food/enforcement.json?api_key=F1QzPnq38QWsSLQzgprkG2UGycgQsqF7QMw4UWx6&
 //search=product_description:"ice"&limit=25
