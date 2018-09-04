58e29a5587d84a013c4717762d3ed4cc262fca62
Brian O'Carroll <bocarroll36@gmail.com>
2018-09-04 10:51:17 -0700

    removed extra comments and debuggers

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const URL_FDA = "https://api.fda.gov/food/enforcement.json";
 const URL_FLICKR = "https://api.flickr.com/services/rest?jsoncallback=?"; 
 const URL_GOOGLE = 'https://www.googleapis.com/customsearch/v1'
-//change product description to user input
 let JSONRequest = "https://api.fda.gov/food/enforcement.json?api_key:&search=product_description:\"banana\"&limit=25"
 let skip = 0;
 function getDataFromRecallAPI(searchTerm, callback) {
-    console.log(searchTerm)
     const recallQuery = {
     url: URL_FDA,
     data: {
@@ -50,19 +48,11 @@ function renderGoogleImage(result) {
 }
 
 function displayRecallData(data) {
-    console.log(data)
-    /*for (let i =0; i<data.results[i];i++) {
-        renderResults(data.results[i])
-    }*/
     const result = data.results.map((item, index) => renderRecallResult(item));
     $('.js-results').html(result);
-    //show results result =data.results[0]
-    //set 2 variables, first for initial.
-    //next to iterate it.
 }
 
 function displayGoogleImage(data) {
-    console.log(data);
     if(data.items) {
     const result = data.items.map((item, index) => renderGoogleImage(item));
     $('.js-google-pic').html(result);
@@ -105,31 +95,3 @@ document.body.scrollTop = document.documentElement.scrollTop = 0;
 
 
 
-//https://api.fda.gov/food/event.json?search=outcomes:"serious+injuries"
-//https://api.fda.gov/food/enforcement.json?api_key=F1QzPnq38QWsSLQzgprkG2UGycgQsqF7QMw4UWx6&
-//search=product_description:"ice"&limit=25
-//results.reason_for_recall
-//results.product_quantity
-//results.product_description 
-
-//for getting first google flickr image result
-
- /*function getDataFromFlickrAPI(searchTerm, callback) {
-    const flickrQuery = {
-        api_key = '5f90685a9a26adf1519a23faa40f32a4',
-        method: "flickr.photos.search",
-        content_type: 1,
-        safe_search:1,
-        format: 'json',
-        text: `${searchTerm}`,
-        sort: 'relevance',
-        per_page: 1
-    }
-    $.getJSON(URL_FLICKR, flickrQuery, callback);
- } */
-
- //work on this
-/* function displayFlickrData(data) {
-    const result = data.items.map((item, index) => )
-}
- */
